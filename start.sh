72c50e1a7dcfa7632543d775eb3532cb173dd7b7
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-01-04 22:35:42 +0100

    added mysql dockerfile to init db by creating tables

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ docker rm node-twitter-machine-learning
 docker stop some-mysql
 docker rm some-mysql
 
-docker run --name some-mysql -v /var/lib/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root MYSQL_DATABASE=twitter-machine-learning -d mysql:latest
-docker run --name node-twitter-machine-learning --restart=always --link some-postgres:postgres -d twitter-machine-learning/node
+docker run --name some-mysql -v /var/lib/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=twitter-machine-learning -d twitter-machine-learning/mysql
+docker run --name node-twitter-machine-learning --restart=always --link some-mysql:mysql -d twitter-machine-learning/node
---
+++ tables.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.10.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost:8889
+-- Généré le :  Lun 04 Janvier 2016 à 22:31
+-- Version du serveur :  5.5.38
+-- Version de PHP :  5.5.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `twitter-machine-learning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `hashtag`
+--
+
+CREATE TABLE `hashtag` (
+`id` int(11) NOT NULL,
+  `tweet` int(11) NOT NULL,
+  `text` varchar(139) NOT NULL,
+  `indice_start` int(11) NOT NULL,
+  `indice_end` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=519 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tweet`
+--
+
+CREATE TABLE `tweet` (
+`id` int(11) NOT NULL,
+  `twitter_id` int(11) NOT NULL,
+  `text` varchar(140) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=339 DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `hashtag`
+--
+ALTER TABLE `hashtag`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tweet`
+--
+ALTER TABLE `tweet`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `hashtag`
+--
+ALTER TABLE `hashtag`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=519;
+--
+-- AUTO_INCREMENT pour la table `tweet`
+--
+ALTER TABLE `tweet`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=339;
